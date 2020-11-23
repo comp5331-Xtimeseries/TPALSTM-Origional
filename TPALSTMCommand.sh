e3d99a3496f5e232ffe0992d722fec0e4d46bc8c
Isabella Liu <isabella618033@gmail.com>
2020-11-24 00:33:00 +0800

    changed run bash

--- TPALSTMCommand.sh
+++ TPALSTMCommand.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
+ATTEN=16
+HORIZON=3
+HORIZON_ELEC=3
+BASHSIZE=32
+
 #Traffic train
 
-python3 main.py --mode train  --attention_len 168 --highway 168 --horizon 12 --save_name traffic --data_set traffic --batch_size 32     --mts 1     --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee trafficTrainLog.txt
+#python3 main.py --mode train  --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON --data_set traffic --batch_size $BASHSIZE     --mts 1     --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/traffic     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee trafficTrainLog.txt
 
 #Traffic test
 
-python3 main.py --mode test --attention_len 168 --highway 168 --horizon 12 --save_name traffic --data_set traffic --batch_size 32      --mts 1    --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee trafficTestLog.txt
+#python3 main.py --mode test --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON --data_set traffic --batch_size $BASHSIZE      --mts 1    --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/traffic     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee trafficTestLog.txt
 
 #ExchangeRate train
 
-python3 main.py --mode train --attention_len 168 --highway 168 --horizon 12 --save_name exchange_rate --data_set exchange_rate --batch_size 32      --mts 1    --dropout 0.2     --learning_rate 3e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee exchangeRateTrainLog.txt
+python3 main.py --mode train --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON --data_set exchange_rate --batch_size $BASHSIZE      --mts 1    --dropout 0.2     --learning_rate 3e-3     --model_dir ./models/exchange_rate     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee exchangeRateTrainLog.txt
 
 #ExchangeRate test
 
-python3 main.py --mode test --attention_len 168 --highway 168 --horizon 12 --save_name exchange_rate --data_set exchange_rate --batch_size 32      --mts 1    --dropout 0.2     --learning_rate 3e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee exchangeRateTestLog.txt
+#python3 main.py --mode test --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON --data_set exchange_rate --batch_size $BASHSIZE      --mts 1    --dropout 0.2     --learning_rate 3e-3     --model_dir ./models/exchange_rate     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee exchangeRateTestLog.txt
 
 #Electricity train
 
-python3 main.py --mode train --attention_len 168 --highway 168 --horizon 24 --save_name electricity --data_set electricity --batch_size 32     --mts 1     --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee electricityTrainLog.txt
+#python3 main.py --mode train --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON_ELEC --data_set electricity --batch_size $BASHSIZE     --mts 1     --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/electricity     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee electricityTrainLog.txt
 
 #Electricity test
 
-python3 main.py --mode test --attention_len 168 --highway 168 --horizon 24 --save_name electricity --data_set electricity --batch_size 32     --mts 1     --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee electricityTestLog.txt
+#python3 main.py --mode test --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON_ELEC  --data_set electricity --batch_size $BASHSIZE     --mts 1     --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/electricity     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee electricityTestLog.txt
 
 
 #Solar train
 
-python3 main.py --mode train --attention_len 168 --highway 168 --horizon 12 --save_name solar --data_set solar --batch_size 32   --mts 1       --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee solarTrainLog.txt
+#python3 main.py --mode train --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON --data_set solar --batch_size $BASHSIZE   --mts 1       --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/solar     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee solarTrainLog.txt
 
 #Solar test
 
-python3 main.py --mode test --attention_len 168 --highway 168 --horizon 12--save_name solar --data_set solar --batch_size 32   --mts 1       --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/model     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee solarTestLog.txt
+#python3 main.py --mode test --attention_len $ATTEN --highway $ATTEN --horizon $HORIZON  --data_set solar --batch_size $BASHSIZE   --mts 1       --dropout 0.2     --learning_rate 1e-3     --model_dir ./models/solar     --num_epochs 40     --num_layers 3     --num_units 338 2>&1 | tee solarTestLog.txt
